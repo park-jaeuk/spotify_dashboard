3d0fed616afd77997b9b79c45b7352f9eacdf9c1
park-jaeuk <jwpark3639@gmail.com>
2024-07-25 21:44:19 +0900

    fix: CRUD 과정 재생성

--- dags/sql/artist.py
+++ dags/sql/artist.py
@@ -1,37 +1,57 @@
-def select_artist(bucket_name: str, date: str=None):
+def select_artist(bucket_name: str):
     sql = f"""
     USE SCHEMA PUBLIC;
 
+    drop table artist;
+
+    -- 최종 테이블 생성
     CREATE TABLE IF NOT EXISTS artist(
         id bigint NOT NULL AUTOINCREMENT START 1 INCREMENT 1,
         spotify_artist_id varchar NOT NULL,
         name varchar NOT NULL,
-        type varchar NOT NULL
+        type varchar NOT NULL,
+        PRIMARY KEY (spotify_artist_id)
     );
 
-
+    -- 파일 포맷 생성
     CREATE OR REPLACE FILE FORMAT my_csv_format
         TYPE = 'CSV'
         FIELD_DELIMITER = ','
         FIELD_OPTIONALLY_ENCLOSED_BY = '"'
         SKIP_HEADER = 1;
 
-
+    -- 스테이지 생성
     CREATE OR REPLACE STAGE artist_stage
         STORAGE_INTEGRATION = s3_int
-        URL = 's3://{bucket_name}/transform/spotify/artists/{date}/'
+        URL = 's3://{bucket_name}/transform/spotify/artists/'
         FILE_FORMAT = my_csv_format;
 
+    -- 데이터 확인
+    LIST @artist_stage;
 
-    COPY INTO SPOTIFY.PUBLIC.ARTIST (spotify_artist_id, name, type) 
+    -- 임시 테이블 생성
+    CREATE OR REPLACE TEMPORARY TABLE temp_artist(
+        spotify_artist_id varchar NOT NULL,
+        name varchar NOT NULL,
+        type varchar NOT NULL
+    );
+
+    -- 각 스테이지로부터 임시 테이블에 데이터 로드
+    COPY INTO temp_artist (spotify_artist_id, name, type) 
     FROM (
-        SELECT $1spotify_id, $2name, $3type
+        SELECT $1, $2, $3
         FROM '@artist_stage'
     )
     FILE_FORMAT = my_csv_format
-    ON_ERROR = 'ABORT_STATEMENT'; 
+    ON_ERROR = 'CONTINUE'; 
 
-    COMMIT
+    -- 중복 제거 후 최종 테이블로 데이터 이동
+    INSERT INTO artist (spotify_artist_id, name, type)
+    SELECT DISTINCT spotify_artist_id, name, type
+    FROM temp_artist;
+
+    -- 트랜잭션 커밋
+    COMMIT;
     """
 
     return sql
