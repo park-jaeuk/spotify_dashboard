fe9211bc479b2b4112659d6c1b9c3c2356fde0a1
park-jaeuk <jwpark3639@gmail.com>
2024-07-25 21:47:52 +0900

    refactor: transform_information_tag_csv 함수 리팩토링

--- dags/transform_last_fm_dag.py
+++ dags/transform_last_fm_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.models.variable import Variable
 
 from datetime import datetime
 import pandas as pd
@@ -10,6 +11,7 @@ import logging
 import os
 import glob
 import json
+import numpy as np
 
 from sql import url
 from utils import common_util
@@ -23,70 +25,60 @@ def transform_reviews_csv() -> None:
     for review_json_path in glob.glob(os.path.join(src_dir_path, "*.json")):
         with open(review_json_path, "r", encoding='UTF-8') as review_json:
             review_api = json.load(review_json)
-            reviews = review_api[review_json_path.replace('\\','/').split('/')[-1].replace('.json','')]['reviews']
-            if len(reviews) == 0:
-                continue
-        review_dict = {}
+
         columns = ['spotify_track_id', 'review', 'date', 'likes']
         review_dict = {column : [] for column in columns}
         spotify_track_id = list(review_api.keys())[0]
+        reviews = review_api[spotify_track_id]
         for review in reviews:
             review_dict['spotify_track_id'].append(spotify_track_id)
             review_dict['review'].append(review['review'])
             review_dict['date'].append(review['date'])
             review_dict['likes'].append(review['likes'])
-        review_df = pd.DataFrame(review_dict, columns = columns).drop_duplicates()
+            review_df = pd.DataFrame(review_dict, columns = columns).drop_duplicates()
         dst_dir_path = os.path.join(Directory.TRANSFORM_DIR, 'last_fm/reviews')
         os.makedirs(dst_dir_path, exist_ok=True)
         dst_file_path = os.path.join(dst_dir_path, f"{spotify_track_id}.csv")
         review_df.to_csv(dst_file_path, encoding='utf-8-sig',index=False)
 
-def transform_information_csv() -> None:
-    columns = ['spotify_track_id','listeners', 'length', 'introduction']
-    info_dic = {column: [] for column in columns}
-
+def transform_information_tag_csv() -> None:
+    columns = ['listeners','length', 'genres', 'last_fm_url', 'introduction', 'spotify_track_id']
     src_dir_path = os.path.join(Directory.DOWNLOADS_DIR, f'last_fm/information')
     transform_df = pd.DataFrame(columns = columns)
+
     for info_json_path in glob.glob(os.path.join(src_dir_path, "*.json")):
         with open(info_json_path, "r", encoding='UTF-8') as info_json:
             info_api = json.load(info_json)
-        spotify_track_id = list(info_api.keys())[0]
-        info_dic = info_api[spotify_track_id]
-        info_dic['spotify_track_id'] = spotify_track_id
-        info_dic.pop('genres')
-        info_df = pd.DataFrame([info_dic])
-        transform_df = pd.concat([transform_df,info_df])
+            
+        spotify_track_id = info_json_path.split('/')[-1][:-5]
+        info_api['spotify_track_id'] = spotify_track_id
+        try:
+            tmp = pd.DataFrame(info_api)
+        except:
+            tmp = pd.DataFrame([info_api])
+        transform_df = pd.concat([transform_df, tmp], axis = 0)
+    transform_df = transform_df.drop_duplicates() 
+    transform_df.reset_index(drop=True, inplace=True)
 
-    transform_df = transform_df.drop_duplicates()
-    transform_df.columns = ['spotify_track_id','listeners', 'duration', 'introduction', 'last_fm_url']
-    dst_dir_path = os.path.join(Directory.TRANSFORM_DIR, f'last_fm/information')
+    info_df = transform_df[['spotify_track_id' , 'listeners', 'length', 'last_fm_url', 'introduction']]
+    dst_dir_path = os.path.join(Directory.TRANSFORM_DIR, f'last_fm/information/{Date.US_DATE}')
     os.makedirs(dst_dir_path, exist_ok=True)
-    dst_file_path = os.path.join(dst_dir_path, "total_information.csv")
-    transform_df.to_csv(dst_file_path, encoding='utf-8-sig',index=False)
+    dst_file_path = os.path.join(dst_dir_path, f"total_information.csv")
+    info_df = info_df.drop_duplicates() 
+    info_df.reset_index(drop=True, inplace=True)
+    info_df.to_csv(dst_file_path, encoding='utf-8-sig',index=False)
 
-def transform_tags_csv() -> None:
-    columns = ['spotify_track_id', 'tags']
-    info_dict = {column: [] for column in columns}
-
-    src_dir_path = os.path.join(Directory.DOWNLOADS_DIR, f'last_fm/information')
-    transform_df = pd.DataFrame(columns = columns)
-    for info_json_path in glob.glob(os.path.join(src_dir_path, "*.json")):
-        with open(info_json_path, "r", encoding='UTF-8') as info_json:
-            info_api = json.load(info_json)
-        spotify_track_id = list(info_api.keys())[0]
-        tags = info_api[spotify_track_id]['genres']
-        spotify_track_id_lst = [spotify_track_id] * len(tags)
-
-        info_dict['spotify_track_id'] = spotify_track_id_lst
-        info_dict['tags'] = tags
-        info_df = pd.DataFrame(info_dict, columns = columns)
-        transform_df = pd.concat([transform_df,info_df])
-
-    transform_df = transform_df.drop_duplicates()    
-    dst_dir_path = os.path.join(Directory.TRANSFORM_DIR, f'last_fm/tags')
+    tag_df = transform_df[['spotify_track_id', 'genres']]
+    tag_df.columns = ['spotify_track_id', 'tags']
+    tag_df['tags'].replace('', np.nan, inplace=True)
+    tag_df.dropna(subset=['tags'])
+    dst_dir_path = os.path.join(Directory.TRANSFORM_DIR, f'last_fm/tags/{Date.US_DATE}')
     os.makedirs(dst_dir_path, exist_ok=True)
     dst_file_path = os.path.join(dst_dir_path, f"total_tags.csv")
-    transform_df.to_csv(dst_file_path, encoding='utf-8-sig',index=False)
+    tag_df = tag_df.drop_duplicates() 
+    tag_df.reset_index(drop=True, inplace=True)
+    tag_df.to_csv(dst_file_path, encoding='utf-8-sig',index=False)
+
 
 
 def upload_transform_reviews_csv_to_s3(bucket_name: str) -> None:
@@ -96,15 +88,17 @@ def upload_transform_reviews_csv_to_s3(bucket_name: str) -> None:
     common_util.upload_files_to_s3(filenames=filenames, keys=keys, bucket_name=bucket_name, replace=True)
 
 def upload_transform_information_csv_to_s3(bucket_name: str) -> None:
-    src_path = os.path.join(Directory.TRANSFORM_DIR,'last_fm/information/*.csv')
+    src_path = os.path.join(Directory.TRANSFORM_DIR, f'last_fm/information/{Date.US_DATE}/*.csv')
     filename = glob.glob(src_path)[0]
     key = filename.replace(Directory.AIRFLOW_HOME, "")[1:]
     common_util.upload_file_to_s3(filename=filename, key=key, bucket_name=bucket_name, replace=True)
 
 def upload_transform_tags_csv_to_s3(bucket_name: str) -> None:
-    src_path = os.path.join(Directory.TRANSFORM_DIR,'last_fm/tags/*.csv')
+    src_path = os.path.join(Directory.TRANSFORM_DIR, f'last_fm/tags/{Date.US_DATE}/*.csv')
     filename = glob.glob(src_path)[0]
     key = filename.replace(Directory.AIRFLOW_HOME, "")[1:]
+
+    Variable.delete("shared_list")
     common_util.upload_file_to_s3(filename=filename, key=key, bucket_name=bucket_name, replace=True)
 
 
@@ -122,14 +116,9 @@ with DAG(dag_id="transform_last_fm_dag",
         python_callable=transform_reviews_csv
     )
 
-    transform_information_csv_task = PythonOperator(
-        task_id = "transform_information_csv_task",
-        python_callable=transform_information_csv
-    )
-
-    transform_tags_csv_task = PythonOperator(
-        task_id = "transform_tags_csv_task",
-        python_callable=transform_tags_csv
+    transform_information_tag_csv_task = PythonOperator(
+        task_id = "transform_information_tag_csv_task",
+        python_callable=transform_information_tag_csv
     )
 
     upload_transform_reviews_csv_to_s3_task = PythonOperator(
@@ -174,10 +163,9 @@ with DAG(dag_id="transform_last_fm_dag",
 
     
     
-    start_task >> [transform_reviews_csv_task, transform_information_csv_task, transform_tags_csv_task]
+    start_task >> [transform_reviews_csv_task, transform_information_tag_csv_task]
 
     transform_reviews_csv_task >> upload_transform_reviews_csv_to_s3_task
-    transform_information_csv_task >> upload_transform_information_csv_to_s3_task
-    transform_tags_csv_task >> upload_transform_tags_csv_to_s3_task
+    transform_information_tag_csv_task >> [upload_transform_information_csv_to_s3_task, upload_transform_tags_csv_to_s3_task]
     
     [upload_transform_reviews_csv_to_s3_task, upload_transform_information_csv_to_s3_task, upload_transform_tags_csv_to_s3_task] >> call_trigger_task >> end_task
